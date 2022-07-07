5c6fc5255cc49937b8856ca9ddf0627ce5fe4218
Facu <84508942+faqbb@users.noreply.github.com>
2022-07-07 19:54:32 -0300

    funciona joya el desafio 2

--- desafio2/desafio2.js
+++ desafio2/desafio2.js
@@ -8,11 +8,11 @@ class Container {
             if(fs.existsSync(path)) {
                 let fileData = await fs.promises.readFile(path, 'utf-8')
                 let products = JSON.parse(fileData)
-                console.log(products)
-                console.log(Object.keys(products).lenght)
                 return products
             } else {
-                return []
+                await fs.promises.writeFile(path, '[]')
+                let readNewFile = await fs.promises.readFile(path, 'utf-8')
+                return readNewFile
             }
         }
         catch(error) {
@@ -22,13 +22,12 @@ class Container {
     addProd = async(prod) => {
        try{ 
         let prods = await this.getAll()
-            console.log(prods.lenght)
-                if (prods.lenght===undefined){
+                if (Object.keys(prods).lenght === 0){
                     prod.id=1
                     prods.push(prod)
                     await fs.promises.writeFile(path, JSON.stringify(prods,null,'\t'))
                 } else {
-                    prod.id = prods[prods.lenght-1].id+1
+                    prod.id = Object.keys(prods).length +1
                     prods.push(prod)
                     await fs.promises.writeFile(path, JSON.stringify(prods,null,'\t'))
                 }
@@ -48,9 +47,9 @@ class Container {
     deleteById = async(idprop) => {
         try {
             let prods = await this.getAll()
-                if(prods.getById(idprop)) {
-                    prods.filter( product => product.id !== idprop)
-                    await fs.promises.writeFile(path, JSON.stringify(prods,null,'\t'))    
+                if(prods.find(prod => prod.id === idprop)) {
+                    let filteredProds = prods.filter( product => product.id !== idprop)
+                    await fs.promises.writeFile(path, JSON.stringify(filteredProds,null,'\t'))    
                 } else {
                     console.log('There is not an object using that id')
                 }
@@ -60,7 +59,7 @@ class Container {
     }
     deleteAll = async() => {
         try {
-            await fs.promises.writeFile(path, [])
+            await fs.promises.writeFile(path, '[]')
         } catch(error) {
             console.log(error)
         }
@@ -69,31 +68,16 @@ class Container {
 }
 
 let milanesa = {
-    title: "milanesa",
-    price: 100,
-    thumbnail: "defaultUrl1"
+    "title": "milanesa",
+    "price": 100,
+    "thumbnail": "defaultUrl1"
 }
 
-let papas = {
-    title: "papas",
-    price: 50,
-    thumbnail: "defaultUrl2"
-}
-
-let pescado = {
-    title: "pescado",
-    price: 150,
-    thumbnail: "defaultUrl3"
-}
 
 const productContainer = new Container()
 const environment = async() => {
-    let allProds = await productContainer.getAll()
-    await productContainer.addProd(milanesa)
-    await productContainer.getById(1)
-    await productContainer.deleteById(1)
-    await productContainer.deleteAll()
-    console.log(allProds)
+//    let obj = await productContainer.getById(4)
+//    console.log(obj)
+//    await productContainer.deleteAll()
 }
-
 environment()
